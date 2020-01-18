37feff3622a929c2f7806838dfafeffeb5327ec6
Li Hong <lihongliew@hotmail.com>
2020-01-18 14:48:34 +0000

    audio in codes

--- audioin.py
+++ audioin.py
@@ -1,3 +1,15 @@
-import speech_recognition as s_r
+import speech_recognition as sr
+import pyaudio
 
-print(s_r.__version__)
+print(sr.__version__)
+print(sr.Microphone.list_microphone_names())
+
+r = sr.Recognizer()
+mic = sr.Microphone(device_index=0)
+
+with mic as source:
+    print("Speak!")
+    r.adjust_for_ambient_noise(source)
+    audio = r.listen(source)
+
+print(r.recognize_google(audio))
